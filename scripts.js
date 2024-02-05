438fb07cc4aa4c9648cbbae04a1e393e17b4ecb4
Joel Wiesner <j.wiesner88@gmail.com>
2024-02-04 16:10:12 -0800

    Created a menu items JS file to hold all menu data
    I exported the object array and then imported it into the main scripts.js file. I then mapped over the object array to use the data to create image cards for each donut menu item.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
-alert("Welcome to Sips!")
+import {menu} from "/menu-items.js"
+
+const menuCard = document.getElementById("menu-cards")
+
+const menuItemHtml = menu.map(item => {
+    return `
+    <div class="item-card">
+        <div id="item-fav"><span class="material-symbols-outlined">favorite</span></div>
+        <div id="item-image"><img src="images/${item.itemImage}" alt="${item.imageAlt}"></div>
+        <div id="item-name" class="item-card-text">${item.itemName}</div>
+        <div id="item-price" class="item-card-text">${item.itemPrice}</div>
+        <div id="add-item" class="item-card-text"><span class="material-symbols-outlined">add_circle</span></div>
+    </div>
+    `
+}).join('')
+
+menuCard.innerHTML = menuItemHtml
