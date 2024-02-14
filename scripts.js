2b7117d0bb8c05cf4fc80e0232bd8e7f29abad6b
Joel Wiesner <j.wiesner88@gmail.com>
2024-02-13 17:36:08 -0800

    mapped over menu items to sort between coffee and donuts when coffee or donut button is clicked

--- scripts.js
+++ scripts.js
@@ -14,11 +14,50 @@ favoritesBtn.addEventListener("click", function(){
 })
 coffeeBtn.addEventListener("click", function(){
     menuTitle.innerHTML = "Coffee"
+    includesCoffee() 
 })
+
+function includesCoffee() {
+    const coffeeCards = menu.map(item => {
+        if(item.itemType.includes("coffee")) {
+            return `
+            <div class="item-card">
+                <div id="item-fav"><span class="material-symbols-outlined favorite">favorite</span></div>
+                <div id="item-image"><img src="images/${item.itemImage}" alt="${item.imageAlt}"></div>
+                <div id="item-name" class="item-card-text">${item.itemName}</div>
+                <div id="item-price" class="item-card-text">${item.itemPrice}</div>
+                <div id="add-item" class="item-card-text"><span class="material-symbols-outlined">add_circle</span></div>
+            </div>
+            `
+        }
+    }).join('')
+
+    menuCard.innerHTML = coffeeCards
+}
+
 
 donutsBtn.addEventListener("click", function(){
     menuTitle.innerHTML = "Donuts"
+    includesDount()
 })
+
+function includesDount() {
+    const donutCards = menu.map(item => {
+        if(item.itemType.includes("donut")) {
+            return `
+            <div class="item-card">
+                <div id="item-fav"><span class="material-symbols-outlined favorite">favorite</span></div>
+                <div id="item-image"><img src="images/${item.itemImage}" alt="${item.imageAlt}"></div>
+                <div id="item-name" class="item-card-text">${item.itemName}</div>
+                <div id="item-price" class="item-card-text">${item.itemPrice}</div>
+                <div id="add-item" class="item-card-text"><span class="material-symbols-outlined">add_circle</span></div>
+            </div>
+            `
+        }
+    }).join('')
+
+    menuCard.innerHTML = donutCards
+}
 
 const menuItemHtml = menu.map(item => {
     return `
